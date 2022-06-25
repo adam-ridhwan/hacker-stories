ddd2c95d8ccab64a61dcf054701b738220b4ab41
Adam Ridhwan <adamridhwan.1@gmail.com>
2022-06-25 14:00:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
-function App() {
+
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = useState(
+    localStorage.getItem(key) || initialState
+  );
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walker',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abromov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ];
+
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
+  );
+
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
+
+      <hr />
+
+      <List list={searchedStories} />
     </div>
   );
-}
+};
+
+
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  type = "text",
+  onInputChange
+}) => (
+  <>
+    <label htmlFor={id}>{label}</label> &nbsp;
+    <input
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
+    />
+  </>
+);
+
+
+const List = ({ list }) =>
+  list.map(({ objectID, ...item }) =>
+    <Item key={objectID} {...item} />
+  );
+
+
+const Item = ({
+  title,
+  url,
+  author,
+  num_comments,
+  points,
+}) => (
+  <div>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </div>
+);
+
 
 export default App;
